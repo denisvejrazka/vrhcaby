4c126710b61122d55188c240bc7758962468f83e
radeksmejky9 <71386098+radeksmejky9@users.noreply.github.com>
2023-04-30 16:21:19 +0200

    base paint finished

--- TestVersion.py
+++ TestVersion.py
@@ -16,13 +16,13 @@ class GameBoard:
         self._base_margin = 30
         self._box_color = (171, 117, 46)
         self._surface_color = (247, 236, 200)
-        self.screen = pg.display.set_mode(
+        self._screen = pg.display.set_mode(
             (self._screen_width, self._screen_height))
-        self.screen.fill(self._box_color)
+        self._screen.fill(self._box_color)
 
     def paint(self):
         for tile in self._tiles:
-            tile.paint(self.screen)
+            tile.paint(self._screen)
 
     def move_stone(self, tile_from, tile_to):
         tile_to.add_stone(tile_from._stones.last())
@@ -62,26 +62,75 @@ class Tile:
 
 
 class Stone:
-    def __init__(self, color):
+    def __init__(self, player):
         self._white_color = (255, 255, 255)
         self._black_color = (0, 0, 0)
-        self._color = color
+        self._player = player
         self._base_radius = 40
 
     def paint(self, board, pos_x, pos_y, num, pos_x_shift, direction):
         y_shifted = (pos_y + (2*self._base_radius)*(num)+self._base_radius) if direction else (
             pos_y + (2*self._base_radius)*(-num)-self._base_radius)
-        color = self._white_color if self._color is "White" else self._black_color
+        color = self._white_color if self._player._color is "White" else self._black_color
         pg.draw.circle(board, color, (pos_x + pos_x_shift,
                        y_shifted), self._base_radius)
 
 
 class Dice:
-    def __init__(self):
-        self._random_number = None
+    def __init__(self, pos_x, pos_y):
+        self._number = 1
+        self._base_size = 100
+        self._dot_base_size = 10
+        self._pos_x = pos_x-self._base_size/2
+        self._pos_y = pos_y-self._base_size/2
 
     def throw(self, rand_from, rand_to):
-        self._random_number = random.randint(rand_from, rand_to)
+        self._number = random.randint(rand_from, rand_to)
+
+    def paint(self, board):
+        pg.draw.rect(board, "White", [
+                     self._pos_x, self._pos_y, self._base_size, self._base_size])
+        match self._number:
+            case 1:
+                self.paint_one(board)
+            case 2:
+                self.paint_two(board)
+            case 3:
+                self.paint_one(board)
+                self.paint_two(board)
+            case 4:
+                self.paint_two(board)
+                self.paint_four(board)
+            case 5:
+                self.paint_one(board)
+                self.paint_two(board)
+                self.paint_four(board)
+            case 6:
+                self.paint_four(board)
+                self.paint_two(board)
+                self.paint_six(board)
+
+    def paint_one(self, board):
+        pg.draw.circle(board, "Black", [
+                       self._pos_x + self._base_size/2, self._pos_y + self._base_size/2], self._dot_base_size)
+
+    def paint_two(self, board):
+        pg.draw.circle(board, "Black", [
+                       self._pos_x + self._base_size/5, self._pos_y + self._base_size/5], self._dot_base_size)
+        pg.draw.circle(board, "Black", [
+                       self._pos_x + self._base_size/5*4, self._pos_y + self._base_size/5*4], self._dot_base_size)
+
+    def paint_four(self, board):
+        pg.draw.circle(board, "Black", [
+                       self._pos_x + self._base_size/5, self._pos_y + self._base_size/5*4], self._dot_base_size)
+        pg.draw.circle(board, "Black", [
+                       self._pos_x + self._base_size/5*4, self._pos_y + self._base_size/5], self._dot_base_size)
+
+    def paint_six(self, board):
+        pg.draw.circle(board, "Black", [
+                       self._pos_x + self._base_size/5, self._pos_y + self._base_size/2], self._dot_base_size)
+        pg.draw.circle(board, "Black", [
+                       self._pos_x + self._base_size/5*4, self._pos_y + self._base_size/2], self._dot_base_size)
 
 
 class Player:
@@ -107,31 +156,31 @@ class Turn:
 player1 = Player("Player1", "White")
 player2 = Player("Player2", "Black")
 game_board = GameBoard(player1, player2)
+dices = [Dice(game_board._screen_width/12*11, game_board._screen_height/2),
+         Dice(game_board._screen_width/12*10, game_board._screen_height/2)]
+tiles = game_board._tiles
 for pos_y in range(2):
     for pos_x in range(13):
         if pos_x is 6:
             continue
         game_board._tiles.append(
-            Tile(pos_x*game_board._screen_width/13,
+            Tile(pos_x*game_board._screen_width/13 if pos_y is 0 else game_board._screen_width-(pos_x+1)*game_board._screen_width/13,
                  game_board._screen_height if pos_y is 1 else 0,
                  game_board._screen_width/13,
                  (player1._color if pos_x % 2 is 0 else player2._color) if pos_y is 0 else player1._color if pos_x % 2 is 1 else player2._color))
-for columns in range(0, 19, 6):
-    for rows in range(5):
-        game_board._tiles[columns].add_stone(
-            Stone(player2._color if columns % 18 is 0 else player1._color))
 
-for rows in range(3):
-    game_board._tiles[5].add_stone(
-        Stone(player2._color))
-    game_board._tiles[17].add_stone(
-        Stone(player1._color))
+for column in range(5):
+    tiles[0].add_stone(Stone(player1))
+    tiles[6].add_stone(Stone(player2))
+    tiles[17].add_stone(Stone(player1))
+    tiles[23].add_stone(Stone(player2))
+for column in range(3):
+    tiles[4].add_stone(Stone(player2))
+    tiles[19].add_stone(Stone(player1))
+for column in range(2):
+    tiles[11].add_stone(Stone(player1))
+    tiles[12].add_stone(Stone(player2))
 
-for rows in range(2):
-    game_board._tiles[11].add_stone(
-        Stone(player1._color))
-    game_board._tiles[23].add_stone(
-        Stone(player2._color))
 
 # General PyGame setup
 pg.init()
@@ -143,6 +192,10 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+
+    for dice in dices:
+        dice.paint(game_board._screen)
+        dice.throw(0, 7)
 
     game_board.paint()
     pg.display.flip()
