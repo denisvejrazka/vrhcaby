eba50d9c5bac7d8dc2f290e5446108a7819653b1
radeksmejky9 <71386098+radeksmejky9@users.noreply.github.com>
2023-06-04 15:20:23 +0200

    format

--- winUI.py
+++ winUI.py
@@ -2,13 +2,13 @@ import tkinter as tk
 import os
 from threading import Timer
 import datetime
+
 background_color = "#C4A484"
 back_block_color = "#add8e6"
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 class WinWindow:
-
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Game Over!")
@@ -18,7 +18,9 @@ class WinWindow:
     def run(self):
         self.window.mainloop()
 
+
 win_ui = WinWindow()
+
 
 class CustomButton:
     button = None
@@ -46,21 +48,22 @@ class CustomButton:
 
 
 class Text:
-
-    def __init__(self, parent, height ,text, color, size, font_style, x, y):
+    def __init__(self, parent, height, text, color, size, font_style, x, y):
         self.label = tk.Label(parent, height=height, fg=color, text=text, font=("Default", size, font_style))
         self.label.pack(padx=20, pady=20)
         self.label.place(relx=x, rely=y, anchor=None)
         self.label.config(background=back_block_color)
 
-                                                # kterej hráč zvítězil
+        # kterej hráč zvítězil
+
+
 First_level_heading = Text(win_ui.window, 3, "Hráč zvítězil!", "White", 40, "bold", None, None)
 text = Text(win_ui.window, 2, "Tentokrát se ti to nepovedlo...", "White", 20, "normal", None, None)
 footer = Text(win_ui.window, 2, "Vrhcáby", "Grey", 14, "normal", 0.45, 0.95)
-date = Text(win_ui.window, 2, datetime.datetime.now().strftime('%H:%M:%S'), "Grey", 14, "normal", 0.90, 0.95)
+date = Text(win_ui.window, 2, datetime.datetime.now().strftime("%H:%M:%S"), "Grey", 14, "normal", 0.90, 0.95)
 
 
-btn = CustomButton("PlayButton", "PlaybuttonDown", win_ui.window, 0.30, 0.55, None) #OK button
-btn2 = CustomButton("PlayButton", "PlaybuttonDown", win_ui.window, 0.70, 0.55, None) #Repeat button
+btn = CustomButton("PlayButton", "PlaybuttonDown", win_ui.window, 0.30, 0.55, None)  # OK button
+btn2 = CustomButton("PlayButton", "PlaybuttonDown", win_ui.window, 0.70, 0.55, None)  # Repeat button
 
 win_ui.run()
